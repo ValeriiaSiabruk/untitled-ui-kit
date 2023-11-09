a40babdab743f34c0f9a40574152cd336ec0739f
Valeriia Styfurak <valeriiastyfurak@gmail.com>
2023-11-09 11:19:46 +0200

    fix dist

--- dist/blog-post.faa1f176.js
+++
@@ -1,2 +0,0 @@
-(()=>{const e=document.querySelector("[data-open-menu-button]"),t=document.querySelector("[data-close-menu-button]"),n=document.querySelector("[data-mobile-menu]");e.addEventListener("click",(()=>{n.classList.add("is-open")})),t.addEventListener("click",(()=>{n.classList.remove("is-open")}))})();
-//# sourceMappingURL=blog-post.faa1f176.js.map
