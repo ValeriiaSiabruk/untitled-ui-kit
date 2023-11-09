a9f8864b7e6f85639c841c50b50cea44571703d6
Valeriia Styfurak <valeriiastyfurak@gmail.com>
2023-11-09 13:47:34 +0200

    add loader to fix bug with svg

---
+++ parcel.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
