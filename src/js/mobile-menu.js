551e205acbf20524bf4cc9c22d18a58f74aa96ac
Valeriia Styfurak <valeriiastyfurak@gmail.com>
2023-11-20 16:21:21 +0200

    add overflow-hidden for body

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -2,12 +2,15 @@
   const openMenuBtnRef = document.querySelector('[data-open-menu-button]');
   const closeMenuBtnRef = document.querySelector('[data-close-menu-button]');
   const mobileMenuRef = document.querySelector('[data-mobile-menu]');
+  const body = document.querySelector('body');
 
   openMenuBtnRef.addEventListener('click', () => {
     mobileMenuRef.classList.add('is-open');
+    body.classList.add('overflow-hidden');
   });
 
   closeMenuBtnRef.addEventListener('click', () => {
     mobileMenuRef.classList.remove('is-open');
+    body.classList.remove('overflow-hidden');
   });
 })();
